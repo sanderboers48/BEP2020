20700b4528cc813910797953cf9db3bac247da68
Sander Boers <shboers@tudelft.nl>
2020-11-12 09:36:43 +0100

    DelfiaFromCenter added

--- MAINplotter.m
+++ MAINplotter.m
@@ -3,7 +3,7 @@ clear
 clc
 close all
 
-makeGIF = false;
+makeGIF = true;
 
 fs = 24;        % Font size in the figure
 avgMs = 10;     % Average marker size
@@ -34,10 +34,12 @@ set(gca,'DataAspectRatio',[1 1 1],'Box','on','FontSize',fs)
 xlabel('x(m)')
 ylabel('y(m)')
 for ii=1:1:length(t)    % Go through time from 0 to tfinal
+    display(ii);
     h = zeros(7,1);
     hh = zeros(1,n);
     kk = 1;
     plot(xPos(ii,1:n),yPos(ii,1:n),'k.','MarkerSize',ms,'HandleVisibility','off')
+    
     for jj = 1:n
         hh(:,jj) = plot_vehicle(xPos(ii,jj), yPos(ii,jj), theta(ii,jj), 2);
     end
@@ -53,6 +55,7 @@ for ii=1:1:length(t)    % Go through time from 0 to tfinal
     % At time 0, draw initial configuration and creat initial gif
     if ii == 1
         h1 = plot(xPos(ii,1:n),yPos(ii,1:n),'bs','MarkerSize',ms+2);
+        plot(0,0,'MarkerSize',ms+2);
         legend(h1,'initial position','Location','NorthEast')
         if makeGIF
             f = getframe(gcf);
@@ -98,151 +101,3 @@ for ii=1:1:length(t)    % Go through time from 0 to tfinal
     delete(hh)
 end
 
-%% Plot trajectory
-figure
-hold on
-xlim([-1.05-20,1+20])
-ylim([-0.9-20,1.35+20])
-set(gca,'DataAspectRatio',[1 1 1],'Box','on','FontSize',fs)
-for ii=1:1:length(t)
-    for jj = 1:n
-        plot(xPos(ii,jj),yPos(ii,jj),'k.','MarkerSize',avgMs)
-        if ii == 1
-           h1 = plot(xPos(ii,jj),yPos(ii,jj),'rs','MarkerSize',1.4*avgMs,...
-                     'LineWidth',lw);
-        end
-        if ii == length(t)
-           h2 = plot(xPos(ii,jj),yPos(ii,jj),'ro','MarkerSize',1.4*avgMs,...
-                     'LineWidth',lw);
-        end
-    end
-    if (ii==1)||(ii==length(t))
-        for jj = 1:n
-            h = plot_vehicle(xPos(ii,jj),yPos(ii,jj),theta(ii,jj),L);
-        end
-%         for i0 = 1:n-1
-%             for j0 = i0+1:n
-%                 if Adj(i0,j0) == 1 && (ii==length(t))
-%                    line([qhx(ii,i0) qhx(ii,j0)],[qhy(ii,i0),qhy(ii,j0)],...
-%                     'LineWidth',lw)
-%                 end
-%             end
-%         end
-    end
-    pause(0.05) % Uncomment this if animation is needed
-end
-legend([h1,h2],'Initial position','Final position','Location','NorthEast')
-xlabel('x (m)')
-ylabel('y (m)')
-%% Plot distance error %%
-nn = 1:8:length(t); % Plot distance error (e) and velocity tracking error 
-                    % (s) using less points
-figure
-subplot(211)
-hold on
-set(gca,'Box','on','FontSize',fs)
-plot(t(nn),e12(nn),'-+','LineWidth',lw,'MarkerSize',1.2*avgMs,...
-     'Color',[0 0 1]);
-plot(t(nn),e23(nn),'-o','LineWidth',lw,'MarkerSize',0.8*avgMs,...
-     'Color',[0 0.5 0]);
-plot(t(nn),e34(nn),'-*','LineWidth',lw,'MarkerSize',1.2*avgMs,...
-     'Color',[1 0 0]);
-plot(t(nn),e45(nn),'.-','LineWidth',lw,'MarkerSize',2.0*avgMs,...
-     'Color',[0 0 0]);
-plot(t(nn),e15(nn),'-x','LineWidth',lw,'MarkerSize',1.4*avgMs,...
-     'Color',[0.75 0 0.75]);
-xlabel('Time (s)')
-ylabel('${e}_{ij}\mbox{ (m)}$','Interpreter','latex')
-h1 = legend('${e}_{12}$','${e}_{23}$','${e}_{34}$',...
-        '${e}_{45}$','${e}_{15}$','Location','SouthEast');
-set(h1,'Interpreter','latex')
-xlim([0 10])
-ylim([-0.4 0.1])
-subplot(212)
-hold on
-set(gca,'Box','on','FontSize',fs)
-plot(t(nn),e13(nn),'-+','LineWidth',lw,'MarkerSize',1.2*avgMs,...
-     'Color',[0 0 1]);
-plot(t(nn),e14(nn),'-o','LineWidth',lw,'MarkerSize',0.8*avgMs,...
-     'Color',[0 0.5 0]);
-plot(t(nn),e24(nn),'-*','LineWidth',lw,'MarkerSize',1.2*avgMs,...
-     'Color',[1 0 0]);
-plot(t(nn),e25(nn),'.-','LineWidth',lw,'MarkerSize',2.0*avgMs,...
-     'Color',[0 0 0]);
-plot(t(nn),e35(nn),'-x','LineWidth',lw,'MarkerSize',1.4*avgMs,...
-     'Color',[0.75 0 0.75]);
-xlabel('Time (s)')
-ylabel('${e}_{ij}\mbox{ (m)}$','Interpreter','latex')
-h2 = legend('${e}_{13}$','${e}_{14}$','${e}_{24}$',...
-        '${e}_{25}$','${e}_{35}$','Location','SouthEast');
-set(h2,'Interpreter','latex')
-xlim([0 10])
-ylim([-0.6 0.15])
-
-
-%% Plot the actual control input %%
-figure
-subplot(211)
-hold on
-set(gca,'Box','on','FontSize',fs)
-plot(t(nn),ubar(1,nn),'LineWidth',1);
-plot(t(nn),ubar(3,nn),'LineWidth',1);
-plot(t(nn),ubar(5,nn),'LineWidth',1);
-plot(t(nn),ubar(7,nn),'LineWidth',1);
-plot(t(nn),ubar(9,nn),'LineWidth',1);
-xlim([0 10])
-ylim([-2.5 2])
-xlabel('Time (s)')
-ylabel('$\bar{u}_{ix}\mbox{ (N)}$','Interpreter','latex')
-grid
-
-subplot(212)
-hold on
-set(gca,'Box','on','FontSize',fs)
-plot(t(nn),ubar(2,nn),'LineWidth',1);
-plot(t(nn),ubar(4,nn),'LineWidth',1);
-plot(t(nn),ubar(6,nn),'LineWidth',1);
-plot(t(nn),ubar(8,nn),'LineWidth',1);
-plot(t(nn),ubar(10,nn),'LineWidth',1);
-xlim([0 10])
-ylim([-0.35 0.35])
-xlabel('Time (s)')
-ylabel('$\bar{u}_{iy}\mbox{ (N-m)}$','Interpreter','latex')
-grid
-
-%% Plot error of estimation dynamics
-for ii = 1:n
-    figure(ii+4)
-    hold on
-    set(gca,'Box','on','FontSize',fs)
-    plot(t(nn),phihat(nn,6*ii-5),'-+','LineWidth',lw,...
-        'MarkerSize',1.2*avgMs,'Color',[0 0 1]);
-    plot(t(nn),phihat(nn,6*ii-4),'-o','LineWidth',lw,...
-        'MarkerSize',0.8*avgMs,'Color',[0 0.5 0]);
-    plot(t(nn),phihat(nn,6*ii-3),'-*','LineWidth',lw,...
-        'MarkerSize',1.2*avgMs,'Color',[1 0 0]);
-    plot(t(nn),phihat(nn,6*ii-2),'.-','LineWidth',lw,...
-        'MarkerSize',2*avgMs,'Color',[0 0 0]);
-    plot(t(nn),phihat(nn,6*ii-1),'-x','LineWidth',lw,...
-        'MarkerSize',1.4*avgMs,'Color',[0.75 0 0.75]);
-    plot(t(nn),phihat(nn,6*ii),'--','LineWidth',lw,'Color',[0 0 0]);
-    xlabel('Time (s)')
-    ylabel(['$\hat{\phi}_{',num2str(ii),'}$'],'Interpreter','latex')
-    h = legend(['$[\hat{\phi}_{',num2str(ii),'}]_{1}$'],...
-               ['$[\hat{\phi}_{',num2str(ii),'}]_{2}$'],...
-               ['$[\hat{\phi}_{',num2str(ii),'}]_{3}$'],...
-               ['$[\hat{\phi}_{',num2str(ii),'}]_{4}$'],...
-               ['$[\hat{\phi}_{',num2str(ii),'}]_{5}$'],...
-               ['$[\hat{\phi}_{',num2str(ii),'}]_{6}$'],...
-               'Location','NorthEast');
-    set(h,'Interpreter','latex')
-end
-
-% Plot errors again
-figure
-plot(t(nn),e12(nn),t(nn),e23(nn),t(nn),e34(nn),t(nn),e45(nn),...
-     t(nn),e15(nn),t(nn),e13(nn),t(nn),e14(nn),t(nn),e24(nn),...
-     t(nn),e25(nn),t(nn),e35(nn),'LineWidth',1);
-xlabel('Time')
-ylabel('e_i_j')
-grid
